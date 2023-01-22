04793844b3ad4f433805a5e3b8c426fd814c85db
Комисар Качалкин <major7577@gmail.com>
2023-01-22 01:32:31 +0000

    BookPage

--- Src/Components/Export.ts
+++ Src/Components/Export.ts
@@ -2,10 +2,10 @@ import { Top } from './Top/Top';
 import { SingUp } from './SingUp/SingUp';
 import { NavbarButton } from './NavbarButton/NavbarButton';
 import { Modal } from './Modal/Modal';
-import { Main } from './Main/Main';
+import { EBooksPage } from '../pages/EBooksPage/EBooksPage';
 import { Filter } from './Filter/Filter';
-import { BookList } from './BookList/BookList';
-import { BookElement } from './BookElement/BookElement';
-import { Navbar } from './NavBar/Navbar';
+import { EBookList } from './EBookList/EBookList';
+import { EBookElement } from './EBookElement/EBookElement';
+import { Navbar } from '../NavBarRouter/Navbar';
 import { SubmitButton } from './SubmitButton/SubmitButton';
-export {Navbar, BookElement, BookList, Filter, Main, Modal, NavbarButton, SingUp, SubmitButton, Top}
+export {Navbar, EBookElement, EBookList, Filter, EBooksPage, Modal, NavbarButton, SingUp, SubmitButton, Top}
